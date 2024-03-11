551bb61440166301d048fd512899e9ec702d8359
Việt Vũ <shuhari0123@gmail.com>
2024-03-11 15:10:39 +0700

    Modifed convertToNewFlightInformation method from class FlightInfromationService to class ConvertUtil

--- src/main/java/com/cgtravelokaservice/service/implement/FlightInformationService.java
+++ src/main/java/com/cgtravelokaservice/service/implement/FlightInformationService.java
@@ -1,38 +1,19 @@
 package com.cgtravelokaservice.service.implement;
 
-import com.cgtravelokaservice.dto.FlightInformationDto;
 import com.cgtravelokaservice.entity.airplant.FlightInformation;
-import com.cgtravelokaservice.repo.AirplaneBrandRepo;
-import com.cgtravelokaservice.repo.AirportLocationRepo;
 import com.cgtravelokaservice.repo.FlightInformationRepo;
-import com.cgtravelokaservice.service.IFlightInformationService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class FlightInformationService implements IFlightInformationService {
-    private final AirplaneBrandRepo airplaneBrandRepo;
+public class FlightInformationService {
 
-    private final AirportLocationRepo airportLocationRepo;
     private final FlightInformationRepo flightInformationRepo;
 
 
-    public FlightInformationService(AirplaneBrandRepo airplaneBrandRepo, AirportLocationRepo airportLocationRepo, FlightInformationRepo flightInformationRepo) {
-        this.airplaneBrandRepo = airplaneBrandRepo;
-        this.airportLocationRepo = airportLocationRepo;
+    public FlightInformationService(FlightInformationRepo flightInformationRepo) {
         this.flightInformationRepo = flightInformationRepo;
     }
 
-    @Override
-    public FlightInformation convertToNewFlightInformation(FlightInformationDto flightInformationDto) {
-        FlightInformation flightInformation = new FlightInformation();
-        flightInformation.setStartTime(flightInformationDto.getStartTime());
-        flightInformation.setEndTime(flightInformationDto.getEndTime());
-        flightInformation.setFromAirPortLocation(airportLocationRepo.getReferenceById(flightInformationDto.getFromAirportLocationId()));
-        flightInformation.setToAirPortLocation(airportLocationRepo.getReferenceById(flightInformationDto.getToAirportLocationId()));
-        flightInformation.setAirPlantBrand(airplaneBrandRepo.getReferenceById(flightInformationDto.getAirplaneBrandId()));
-
-        return flightInformation;
-    }
     public void saveFlightInformation(FlightInformation flightInformation) {
         flightInformationRepo.save(flightInformation);
     }
