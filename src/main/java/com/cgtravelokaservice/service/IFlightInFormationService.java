e726a1d33916df2b5f2bd59c7501ff8c06d5e1dd
LamChiHung <146713547+LamChiHung@users.noreply.github.com>
2024-03-14 14:15:49 +0700

    fix error can't stage

--- src/main/java/com/cgtravelokaservice/service/IFlightInFormationService.java
+++
@@ -1,4 +0,0 @@
-package com.cgtravelokaservice.service;
-
-public interface IFlightInFormationService {
-}
---
+++ src/main/java/com/cgtravelokaservice/service/IFlightInformationService.java
@@ -0,0 +1,20 @@
+package com.cgtravelokaservice.service;
+
+import com.cgtravelokaservice.dto.FlightInfoSearchDTO;
+import com.cgtravelokaservice.dto.request.SearchFlightDetailsRequestDTO;
+import com.cgtravelokaservice.dto.response.SearchFlightResponse;
+import com.cgtravelokaservice.entity.airplant.FlightInformation;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+
+import java.util.List;
+
+public interface IFlightInformationService {
+    void saveFlightInformation(FlightInformation flightInformation);
+
+    Slice <FlightInfoSearchDTO> searchFlights(SearchFlightDetailsRequestDTO request, Pageable pageable);
+
+    List <FlightInformation> searchList(SearchFlightDetailsRequestDTO request);
+
+    SearchFlightResponse loadSearchFlightResponse(SearchFlightDetailsRequestDTO request);
+}
