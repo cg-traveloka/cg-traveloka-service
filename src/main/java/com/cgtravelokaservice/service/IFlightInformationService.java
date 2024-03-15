088a5b44533230c2270ceaf206ba4689733a8ece
dinhhieu240901 <hieun4587@gmail.com>
2024-03-15 09:24:19 +0700

    push code branch hieu

--- src/main/java/com/cgtravelokaservice/service/IFlightInformationService.java
+++ src/main/java/com/cgtravelokaservice/service/IFlightInformationService.java
@@ -1,8 +1,7 @@
 package com.cgtravelokaservice.service;
 
-import com.cgtravelokaservice.dto.DetailedFlightInformationDto;
-import com.cgtravelokaservice.dto.FlightDetailsDTO;
-import com.cgtravelokaservice.dto.request.FlightDetailsRequestDTO;
+import com.cgtravelokaservice.dto.FlightInfoSearchDTO;
+import com.cgtravelokaservice.dto.request.SearchFlightDetailsRequestDTO;
 import com.cgtravelokaservice.dto.response.SearchFlightResponse;
 import com.cgtravelokaservice.entity.airplant.FlightInformation;
 import org.springframework.data.domain.Pageable;
@@ -10,7 +9,9 @@ import org.springframework.data.domain.Slice;
 
 import java.util.List;
 
+
 public interface IFlightInformationService {
+
     void saveFlightInformation (FlightInformation flightInformation);
     Slice<FlightInfoSearchDTO> searchFlights (SearchFlightDetailsRequestDTO request, Pageable pageable);
 
