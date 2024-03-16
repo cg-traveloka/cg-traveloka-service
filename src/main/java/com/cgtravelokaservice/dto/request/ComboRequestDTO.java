3176ad9204794f54b32a23a4faef3b8a39fc795d
LamChiHung <146713547+LamChiHung@users.noreply.github.com>
2024-03-16 21:38:17 +0700

    change search flights api uri

---
+++ src/main/java/com/cgtravelokaservice/dto/request/ComboRequestDTO.java
@@ -0,0 +1,14 @@
+package com.cgtravelokaservice.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ComboRequestDTO {
+    private SearchFlightDetailsRequestDTO
+            searchFlightDetailsRequestDTO;
+    private HotelSearchDTO hotelSearchDTO;
+}
